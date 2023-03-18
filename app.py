212ab10772c3f6c13d3ce962156124f75d95d63d
Sam <youngsamsy.1@gmail.com>
2023-03-18 23:07:26 +0100

    16 commit

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from api import create_app
 from flask_sqlalchemy import SQLAlchemy
 from api.config.config import config_dict
 
-app = create_app(config=config_dict['prod'])
-db = SQLAlchemy
+app = create_app
+
+db = SQLAlchemy(app)
 if __name__=="__main__":
     app.run(debug=True)
