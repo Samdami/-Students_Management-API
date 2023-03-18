ad02e2f85c2e6f2bfca3c979353e4607ee6c3eeb
Sam <youngsamsy.1@gmail.com>
2023-03-18 19:31:19 +0100

    16 commit

--- api/config/config.py
+++ api/config/config.py
@@ -5,15 +5,15 @@ import re
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# db_name = "student_api_db"
+db_name = 'studentapi_db'
 
-# default_uri = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', db_name)
+default_uri = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', db_name)
 
-# uri = os.getenv('DATABASE_URL', default_uri) # or other relevant config var
-# if uri.startswith('postgres://'):
-#     uri = uri.replace('postgres://', 'postgresql://', 1)
+uri = os.getenv('DATABASE_URL', default_uri) # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
 
-DATABASE_URL = os.environ.get('postgres://hywjcndkxckgre:1b1dc1a76e4fb939e3c605ff985114fd3bb4f3d404832bf9bcd72eaaaeb1904f@ec2-52-205-45-222.compute-1.amazonaws.com:5432/dfjvm6n0apg6oc')
+# DATABASE_URL = os.environ.get('postgres://hywjcndkxckgre:1b1dc1a76e4fb939e3c605ff985114fd3bb4f3d404832bf9bcd72eaaaeb1904f@ec2-52-205-45-222.compute-1.amazonaws.com:5432/dfjvm6n0apg6oc')
 
 
 class Config:
@@ -36,7 +36,7 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = config('DEBUG', False, cast=bool)
     
